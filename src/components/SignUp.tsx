8c3a4b68b01702f7227f7b08a16e1055cd1444e5
9re9o <gregorymena16@gmail.com>
2023-06-08 22:44:24 -0600

    adding somes files

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,51 @@
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { auth } from '@/firebase/app'
+
+const SignUp: React.FC = () => {
+	const [signUpForm, setSignUpForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignUpForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+const handleSubmit = () => {}
+
+return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        required
+        name="email"
+        placeholder="email"
+        type="email"
+        onChange={handleChange}
+      />
+      <Input
+        required
+        name="password"
+        placeholder="password"
+        type="password"
+        onChange={handleChange}
+      />
+      <Input
+        required
+        name="confirmPassword"
+        placeholder="Confirm password"
+        type="password"
+        onChange={handleChange}
+      />
+
+      <Button type="submit">
+        Sign Up
+      </Button>
+    </form>
+  )
+}
+
+export default SignUp
